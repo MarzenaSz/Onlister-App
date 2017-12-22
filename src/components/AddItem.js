983c9039182d27aa06e9af05a56cfe4b75c16458
Marzena <marzena_szopinska@hotmail.com>
2017-12-22 19:17:37 +0000

    fixing add item event

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,30 +2,40 @@ import React from 'react';
 
 class AddItem extends React.Component {
 
+    // set default state
     state = {
         error: undefined
     };
 
-    
-    // method resposible for 
-    handleAddItemEvent = (event) => {
-        // prevet full pafe refresh after submiting the form
-        event.preventDefault();
-        // store typed values and remove trailling spaces
-        const shop = event.target.elements.shop_name.value.trim();
-        const item = event.target.elements.item.vlue.trim();
+    handleAddItem = (e) => {
+        // prevent the full page refresh on submit
+        e.preventDefault();
+
+        // check if there is any value typed in and remove any trailing spaces (trim())
+        const item = e.target.elements.item.value.trim();
+
+         // (log data)...pass in item as an argument inside of handleAddItem method.
+        const error = this.props.handleAddItem(item);
+
+        // update data and rerender it
+        this.setState(() => ({ error: error }));
+
+        // if there was no error, clear the ipnut
+        if(!error){
+            e.target.elements.item.value = '';
+        }
         
     };
 
     render() {
         return (
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddItemEvent} className="onlister_form">
-                    <input type="text" name="shop_name"/>
-                    <input type="text" name="item" />
-                </form>
-                <button>Add Item</button>
+                 {/* print error message if there is any*/}
+                 {this.state.error && <p>{this.state.error}</p>}
+                 <form onSubmit={(e) => {this.props.handleAddItem(e.target.elements.item.value)}}>
+                     <input type="text" name="item" />
+                     <button>Add Item</button>
+                 </form>   
             </div>
         );
     }
--- src/components/OnlisterApp.js
+++ src/components/OnlisterApp.js
@@ -11,21 +11,9 @@ class OnlisterApp extends React.Component {
     };
 
     // method responsible for sorting item in items array
-    handleSortItems = () => {
+    //handleSortItems = () => {
 
-    };
-     // method resposible for adding an item to items array
-    handleAddItem = (item) => {
-        // check if item exists
-        if(!item) {
-            return "Enter valid value to add an item";
-        }
-        // check if passed in option already exists
-        else if(this.state.items.indexOf(item) > -1){
-            return "The item under this name already exists";
-
-        }
-    };
+    //};
 
     // method responsible for removing all items from items array
     handleRemoveItems = () => {
@@ -46,6 +34,22 @@ class OnlisterApp extends React.Component {
         }));     
     };
 
+     // method resposible for adding an item to items array
+     handleAddItem = (item) => {
+        // check if item exists
+        if(!item) {
+            return "Enter valid value to add an item";
+            console.log('no item');
+        } 
+        else if(this.state.items.indexOf(item) > -1){
+            return "The item under this name already exists";
+        }
+
+        console.log('I am working!');
+        // merge two arrays thanks to concat and return a new array that contains all of the elements from both arrays
+        this.setState((prevState) => ({ items: prevState.items.concat([item]) })); // <- insert item inside of old array 
+    };
+
     render() {
         // info for Header
         const title = "Onlister App";
@@ -58,7 +62,7 @@ class OnlisterApp extends React.Component {
                 <Header title={title} subtitle={subtitle}/>
                 {/* render Shopping List and pass in state and two methods as props*/}
                 <ShoppingList items={this.state.items} handleRemoveItems={this.handleRemoveItems} handleRemoveItem={this.handleRemoveItem}/>
-                <AddItem />
+                <AddItem handleAddItem={this.handleAddItem}/>
             </div>
         );
     }
